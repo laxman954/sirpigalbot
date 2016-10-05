420a2674aa1e79b2d235d045bf2ebef0d90d12c2
Lekshmana Perumal <lperumalm@paypal.com>
2016-10-05 19:51:16 +0530

    Test root added

--- src/main/java/org/sirpigal/slack/init/SirpigalSlackApplication.java
+++ src/main/java/org/sirpigal/slack/init/SirpigalSlackApplication.java
@@ -2,11 +2,14 @@ package org.sirpigal.slack.init;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author lperumalm
  *
  */
+@RestController
 @EnableAutoConfiguration
 public class SirpigalSlackApplication {
 	/**
@@ -17,6 +20,11 @@ public class SirpigalSlackApplication {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(SirpigalSlackApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Sirpigal Bot!";
 	}
 
 }
