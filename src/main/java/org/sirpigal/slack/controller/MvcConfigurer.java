b9a4668303963af3eb1198a1362f6d156bb32450
Lekshmana Perumal <lperumalm@paypal.com>
2016-10-08 15:25:54 +0530

    Angular js files added

---
+++ src/main/java/org/sirpigal/slack/controller/MvcConfigurer.java
@@ -0,0 +1,25 @@
+package org.sirpigal.slack.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration  
+public class MvcConfigurer extends WebMvcConfigurerAdapter {  
+  
+    @Override  
+    public void addViewControllers(ViewControllerRegistry registry) {  
+        registry.addViewController("/").setViewName("sample.html");  
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);  
+    }  
+  
+    @Override  
+    public void configurePathMatch(PathMatchConfigurer configurer) {  
+        super.configurePathMatch(configurer);  
+        configurer.setUseSuffixPatternMatch(false);  
+    }  
+  
+  
+}  
